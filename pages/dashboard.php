3b12b353f2b96302ffb3cc204867803ea073c03a
Renato Silva <renatoleite.log@gmail.com>
2025-07-26 17:37:29 +1000

    Updated CSS styling

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -14,6 +14,7 @@ if (!isset($_SESSION['user_id'])) {
     
     <link rel="stylesheet" href="../style/style.css">
     <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.10.1/main.min.css' rel='stylesheet' />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Dashboard - Home Delivery Booking</title>
 </head>
 <body>
